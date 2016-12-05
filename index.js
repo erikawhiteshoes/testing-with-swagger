c52ff7826fec3633e12e57c55cb4343e447d491c
erikawhiteshoes <erikawhiteshoes@gmail.com>
2016-12-05 13:51:26 -0500

    completed challenge

--- index.js
+++ index.js
@@ -1,12 +1,109 @@
-var express = require('express');
-var app = express();
+/* jshint esversion:6, asi:true */
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const express      = require('express')
+const app          = express()
+const eventService = require('./services/eventService.js')
+const bodyParser   = require('body-parser')
+const basicAuth    = require('basic-auth')
 
-app.listen(9999, function () {
-  console.log('Example app listening on port 9999!');
-});
+ app.use(bodyParser.urlencoded({extended: true}))
+ app.use(bodyParser.json())
 
-module.exports = app;
+ var auth = function (req, res, next) {
+   function unauthorized(res) {
+     res.set('WWW-Authenticate', 'Basic realm=Authorization Required')
+     return res.sendStatus(401)
+   }
+
+   var user = basicAuth(req)
+   if (!user || !user.name || !user.pass) {
+     return unauthorized(res)
+   }
+
+   if (user.name === 'admin' && user.pass === 'password') {
+     return next()
+   } else {
+     return unauthorized(res)
+   }
+ }
+
+app.get('/', (req, res) => {
+  const package = require('./package.json')
+  res.json({
+    title: "Events API",
+    version: package.version,
+    status: "ok"
+  })
+})
+
+app.get('/events', (req, res) => {
+  eventService.allEvents((err, events) => {
+    if(err) {
+      res.send(err)
+    }
+    res.json({data:events})
+  })
+})
+
+app.post('/events', (req, res) => {
+  let events = req.body.data
+  eventService.newEvents(events, (err, insertedEvents) => {
+    if(err){
+      res.statusCode(501).send(err)
+    }
+    var insertedIds = insertedEvents.map(evt => evt._id )
+    res.status(201)
+       .json({data:insertedEvents, inserted:insertedIds})
+  })
+})
+
+app.get('/events/:event_id', (req, res) => {
+  eventService.findOneEventById(req.params.event_id, (err, event) => {
+    if(err) {
+      res.send(err)
+      return
+    }
+    if(event === null) {
+      res.status(404)
+      res.send('event not found')
+      return
+    }
+    res.json({data:event})
+  })
+})
+
+app.put('/events/:event_id', (req, res) => {
+  let eventId = req.params.event_id
+  let updatedEvent = req.body.data
+  eventService.updateEvent(eventId, updatedEvent, (err, event) => {
+    if(err) {
+      res.send(err)
+    }
+    res.json({replaced:[event._id]})
+  })
+})
+
+app.delete('/events/:event_id', (req, res) => {
+  let deletedId = req.params.event_id
+  eventService.deleteEvent(deletedId, (err, event) => {
+    if(err) {
+      res.send(err)
+    }
+    res.status(202)
+       .json({deleted:[deletedId]})
+  })
+})
+
+app.get('/secrets', auth, (req, res) => {
+  res.json({
+    "secrets": [
+      "The answer is 42."
+    ]
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!')
+})
+
+module.exports = app
---
+++ services/eventService.js
@@ -0,0 +1,37 @@
+/* jshint esversion:6, asi:true */
+
+const Datastore = require('nedb')
+const path = require('path')
+
+const events = new Datastore({ filename: path.join(__dirname, 'events.db')})
+events.loadDatabase()
+
+function allEvents(cb) {
+  events.find({}, cb)
+}
+
+function newEvents(arr, cb) { //cb(err,val)
+  let E = arr.map(obj => ({name: obj.name, start: obj.start, end: obj.end}))
+  events.insert(E, cb)
+}
+
+function findOneEventById(id, cb) {
+  events.findOne({_id: id}, cb)
+}
+
+function updateEvent(id, evts, cb) {
+  evts = evts.length !== undefined ? [evts] : evts
+    events.update({_id: id}, evts, {multi:true, upsert:true, returnUpdatedDocs:true}, (err, numAffected, affectedDocuments, upsert) => {
+      if(err) {console.error(err)}
+      cb(err, affectedDocuments)
+    })
+}
+
+function deleteEvent(id, cb) {
+  events.remove({_id: id}, {}, cb)
+}
+
+
+module.exports = {
+newEvents, allEvents, findOneEventById, updateEvent, deleteEvent
+}
